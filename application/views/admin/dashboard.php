7cd300ea63beb7cc0692c19cc9cc1ae54cedfc61
aldi wirawan <aldiwira.aw@gmail.com>
2021-02-08 22:34:56 +0700

    remake box

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -10,11 +10,8 @@
                 <div class="card bg-primary text-white mb-4">
                     <div class="card-body">
                         <h5 class="font-weight-bold">Jumlah Barang Baru Digudang</h5>
+                        <hr class="bg-white" />
                         <h5>5</h5>
-                    </div>
-                    <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link text-decoration-none" href="#">View Details</a>
-                        <div class="small text-white"><i class="fa fa-angle-right"></i></div>
                     </div>
                 </div>
             </div>
@@ -22,11 +19,8 @@
                 <div class="card bg-success text-white mb-4">
                     <div class="card-body">
                         <h5 class="font-weight-bold">Jumlah Barang Normal Digudang</h5>
+                        <hr class="bg-white" />
                         <h5>5</h5>
-                    </div>
-                    <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link text-decoration-none" href="#">View Details</a>
-                        <div class="small text-white"><i class="fa fa-angle-right"></i></div>
                     </div>
                 </div>
             </div>
@@ -34,11 +28,8 @@
                 <div class="card bg-danger text-white mb-4">
                     <div class="card-body">
                         <h5 class="font-weight-bold">Jumlah Barang Rusak Digudang</h5>
+                        <hr class="bg-white" />
                         <h5>5</h5>
-                    </div>
-                    <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link text-decoration-none" href="#">View Details</a>
-                        <div class="small text-white"><i class="fa fa-angle-right"></i></div>
                     </div>
                 </div>
             </div>
@@ -52,7 +43,7 @@
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-success text-white mb-4">
                     <div class="card-body">
-                        <h5 class="font-weight-bold">Jumlah Barang Normal Digudang</h5>
+                        <h5 class="font-weight-bold">Jumlah catatan masuk</h5>
                         <h5>5</h5>
                     </div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
@@ -64,7 +55,7 @@
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-danger text-white mb-4">
                     <div class="card-body">
-                        <h5 class="font-weight-bold">Jumlah Barang Rusak Digudang</h5>
+                        <h5 class="font-weight-bold">Jumlah catatan keluar</h5>
                         <h5>5</h5>
                     </div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
